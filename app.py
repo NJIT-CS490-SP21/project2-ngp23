46f4356f2239c261050380cac8c7a8999c59beb6
EC2 Default User <ec2-user@ip-172-31-83-156.ec2.internal>
2021-03-10 17:24:28 +0000

    moidy app.py with linting

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
+'''Server side of the app'''
 import os
 from flask import Flask, send_from_directory, json
 from flask_socketio import SocketIO
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv, find_dotenv
-import operator
+
 
 load_dotenv(find_dotenv()) # This is to load your env variables from .env
 
@@ -23,8 +24,8 @@ db.create_all()
 
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
-userList = []
-userNames = {}
+user_list = []
+user_names = {}
 
 
 socketio = SocketIO(
@@ -37,11 +38,13 @@ socketio = SocketIO(
 @app.route('/', defaults={"filename": "index.html"})
 @app.route('/<path:filename>')
 def index(filename):
+    ''' index function with file name '''
     return send_from_directory('./build', filename)
 
 # When a client connects from this Socket connection, this function is run
 @socketio.on('connect')
 def on_connect():
+    ''' function exicutes upon connection with client'''
     print('User connected!')
     all_people = models.Person.query.all()
     users = []
@@ -54,30 +57,37 @@ def on_connect():
 # When a client disconnects from this Socket connection, this function is run
 @socketio.on('disconnect')
 def on_disconnect():
+    ''' function exicutes upon disconnect with client'''
     print('User disconnected!')
 
 # When a client emits the event 'chat' to the server, this function is run
 # 'chat' is a custom event name that we just decided
 @socketio.on('chat')
 def on_chat(data): # data is whatever arg you pass in your emit call on client
+    '''Socket listen for the chat event and if
+       any event occurs it will emit to the client back '''
     print(str(data))
     # This emits the 'chat' event from the server to all clients except for
     # the client that emmitted the event that triggered this function
     socketio.emit('chat', data, broadcast=True, include_self=False)
 @socketio.on('click')
 def on_click(data): # data is whatever arg you pass in your emit call on client
+    '''Listens for click event which updates the board
+     if the event occurs it emits back to the client'''
     print(str(data))
     # This emits the 'chat' event from the server to all clients except for
     # the client that emmitted the event that triggered this function
     socketio.emit('click', data, broadcast=True, include_self=False)
 @socketio.on('reset')
 def on_reset(data): # data is whatever arg you pass in your emit call on client
+    '''Event listner for reset function for the board'''
     print(str(data))
     # This emits the 'chat' event from the server to all clients except for
     # the client that emmitted the event that triggered this function
     socketio.emit('reset', data, broadcast=True, include_self=False)
 #helper function
 def add_db(data): # data is whatever arg you pass in your emit call on client
+    '''functions checks for the data in DB and if not in Db then add to db'''
     check = models.Person.query.filter_by(username=data["setUser"]).first()
     print(check)
     if check is None:
@@ -87,40 +97,38 @@ def add_db(data): # data is whatever arg you pass in your emit call on client
         db.session.commit()
 
 def score_board():
-    scoreDict = []
-    score = models.Person.query.order_by(models.Person.score.desc()).all()
-    for s in score:
-        scoreDict.append({s.username:s.score})
+    ''' functions gets all the players and score from DB and store in list of Dict'''
+    score_dict = []
+    score_people = models.Person.query.order_by(models.Person.score.desc()).all()
+    for score in score_people:
+        score_dict.append({score.username:score.score})
     #sorted_d=dict(sorted(scoreDict.items(),key=operator.itemgetter(1),reverse=True))
-    print("score", scoreDict)
-    return scoreDict
+    print("score", score_dict)
+    return score_dict
 
 
 @socketio.on('login')
 def on_login(data): # data is whatever arg you pass in your emit call on client
-    print("User loggedin",str(data))
+    '''Functions listens for login event and asigns the X, O and spec'''
+    print("User loggedin", str(data))
     add_db(data)
-    if "X" not in userNames:
-        userNames["X"] = data["setUser"]
-    elif "O" not in userNames:
-        userNames["O"] = data["setUser"]
+    if "X" not in user_names:
+        user_names["X"] = data["setUser"]
+    elif "O" not in user_names:
+        user_names["O"] = data["setUser"]
     else:
-        userList.append(data["setUser"])
-        userNames["spec"] = userList
-    print(userNames)
+        user_list.append(data["setUser"])
+        user_names["spec"] = user_list
+    print(user_names)
     score = score_board()
-    socketio.emit('resetStats', score, broadcast=True, include_self=False )
-    socketio.emit('login', userNames, broadcast=True, include_self=False)
-    
-
+    socketio.emit('resetStats', score, broadcast=True, include_self=False)
+    socketio.emit('login', user_names, broadcast=True, include_self=False)
 # Note we need to add this line so we can import app in the python shell
 @socketio.on('resetStats')
-def resetStats(data):
+def reset_stats(data):
+    '''Resets the data after ever game'''
     winner = data["setWin"]
     loser = data["setLose"]
-    print(winner)
-    print(loser)
-    
     db.session.query(models.Person)\
        .filter(models.Person.username == winner)\
        .update({models.Person.score: models.Person.score + 1})
@@ -128,7 +136,7 @@ def resetStats(data):
        .filter(models.Person.username == loser)\
        .update({models.Person.score: models.Person.score - 1})
     db.session.commit()
-    score=score_board()
+    score = score_board()
     print(score)
     socketio.emit('resetStats', score, broadcast=True)
 if __name__ == "__main__":
@@ -138,3 +146,4 @@ if __name__ == "__main__":
         host=os.getenv('IP', '0.0.0.0'),
         port=8081 if os.getenv('C9_PORT') else int(os.getenv('PORT', 8081)),
     )
+    
--- models.py
+++ models.py
@@ -1,9 +1,12 @@
+''' Database schema '''
 from app import db
 
 class Person(db.Model):
+    ''' adds username, score and id colums in the database'''
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     score = db.Column(db.Integer, unique=False, nullable=False)
 
     def __repr__(self):
         return '<Person %r>' % self.username
+        
